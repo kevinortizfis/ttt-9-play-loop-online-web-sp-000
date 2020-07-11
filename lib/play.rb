997d708c0aeab3e1bd3ae7ea857e40df09ae6a42
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-11 22:15:22 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,7 @@ end
 
 # Define your play method below
 def play(board)
-    while board != ["X","X","X","X","X","X","X","X","X"] || board != ["O","O","O","O","O","O","O","O","O"]
+    while board != ["X","X","X","X","X","X","X","X","X"]
       turn(board)
     end
 end
