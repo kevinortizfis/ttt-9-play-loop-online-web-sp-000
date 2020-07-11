2d041c7b20e019cb245a91167c031642df5a2109
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-11 22:17:29 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "O")
+def move(board, index, current_player = "X")
   board[index] = current_player
 end
 
